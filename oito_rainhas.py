cdcde979cb038b19aad4d2e1538e1e58212b57d6
Dudsedu <edumini.mini@gmail.com>
2023-01-09 14:44:20 -0300

    verificação do tabuleiro

--- oito_rainhas.py
+++ oito_rainhas.py
@@ -1,5 +1,25 @@
 def verifica():
-    pass
+    flag = True
+    for linha in range(8):
+        for coluna in range(8):
+            if tabuleiro[linha][coluna] == '1':
+                if tabuleiro[linha].count('1') > 1: #################### Verifica posições horizontais.
+                    flag = False
+                for linha1 in range(8): ################################ Verifica posições verticais.
+                    if linha1 != linha:                 
+                        if tabuleiro[linha1][coluna] == '1':
+                            flag = False
+                        diferenca = abs(linha-linha1) ################## Verifica diagonais.
+                        if coluna-diferenca >= 0:
+                            if tabuleiro[linha1][coluna-diferenca] == '1':
+                                flag = False
+                        if coluna+diferenca <= 7:
+                            if tabuleiro[linha1][coluna+diferenca] == '1':
+                                flag = False
+    if flag is True:
+        return 1
+    else:
+        return 0
 
 def monta(tabuleiro):
     rainhas = 0
