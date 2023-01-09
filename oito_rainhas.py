5203b669a1d8f2263680bd4c2a6e04d125f90c1b
Dudsedu <edumini.mini@gmail.com>
2023-01-09 14:43:14 -0300

    função de entrada

--- oito_rainhas.py
+++ oito_rainhas.py
@@ -2,4 +2,17 @@ def verifica():
     pass
 
 def monta(tabuleiro):
-    pass
+    rainhas = 0
+    if len(tabuleiro) != 8:
+        return -1
+    if len(tabuleiro) == 8:
+        for linha in range(8):
+            for coluna in range(8):
+                if tabuleiro[linha][coluna] == '1':
+                    rainhas += 1
+                if tabuleiro[linha][coluna] != '1' and tabuleiro[linha][coluna] != '0':
+                    return -1
+        if rainhas == 8 and len(tabuleiro) == 8:
+            return verifica(tabuleiro)
+        if rainhas != 8:
+            return -1
